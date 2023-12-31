cb6499b350b5f1439bc70b858d94ef53bd29950a
Ana Karina <eisden.akf@outlook.com>
2023-12-31 23:59:51 +0100

    sidebar toegevoegd aan de gridpane

--- src/main/java/com/anakarina/demowerkroosterbeheersysteem/HelloApplication.java
+++ src/main/java/com/anakarina/demowerkroosterbeheersysteem/HelloApplication.java
@@ -2,7 +2,9 @@ package com.anakarina.demowerkroosterbeheersysteem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,10 +15,22 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         GridPane root = new GridPane();
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root, 1024, 600);
+        scene.getStylesheets().add(HelloApplication.class.getResource("stylesheets/homescreen.css").toString());
+
+
+        FlowPane sidebar = new FlowPane();
+        sidebar.setId("sidebar");
+        sidebar.prefHeightProperty().bind(scene.heightProperty());
+        sidebar.maxHeightProperty().bind(scene.heightProperty());
+        sidebar.setOrientation(Orientation.VERTICAL);
+
+
+        stage.setTitle("Werkrooster Beheer System");
         stage.setScene(scene);
         stage.show();
+
+        root.getChildren().addAll(sidebar);
     }
 
     public static void main(String[] args) {
